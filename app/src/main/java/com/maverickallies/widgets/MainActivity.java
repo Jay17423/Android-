20817fe7d9e12a6ea71ff8622030e3ee67c98ecc
jay17423 <jayprakash1200@gmail.com>
2024-01-11 11:56:00 +0530

    Added new Features like Check Box and Image view

--- app/src/main/java/com/maverickallies/widgets/MainActivity.java
+++ app/src/main/java/com/maverickallies/widgets/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView FirstText;
     ImageView Firstimage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Firstimage=(ImageView) findViewById(R.id.myimage);
     }
     public void onMyButtonClick(View view){
+        CheckBox BasicCheckBox = (CheckBox) findViewById(R.id.basicCheckbox);
         String myName = textview.getText().toString();
         
         if(myName.equals(" ")){
@@ -35,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
         else{
             Firstimage.setVisibility(view.VISIBLE);
         }
+
+        boolean ischecked =BasicCheckBox.isChecked();
+        if(ischecked){
+            Toast.makeText(this, "You will be successfull", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Please accept the terms and condition", Toast.LENGTH_SHORT).show();
+        }
+
+
 
     }
 
